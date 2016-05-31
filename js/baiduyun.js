1491405bed30f9db4872c51fb5d0eb3e9c965b00
Erin580066 <597516068@qq.com>
2016-06-01 00:52:35 +0800

    num3

--- js/baiduyun.js
+++ js/baiduyun.js
@@ -1,4 +1,9 @@
-var btn = document.getElementById("btn");
-btn.onfocus = function(){
-	btn.placeholder = ''
+/////搜索框的焦点事件
+focusevent();
+function focusevent(){
+	var Btn = document.getElementById("btn");
+	Btn.onfocus = function(){
+		Btn.placeholder = ''
+	}
 }
+/////全局右键菜单
